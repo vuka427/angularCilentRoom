a169e007f00b9ee4194494a2d6178898ba228506
vuka427 <atrox427@gmail.com>
2023-11-06 22:28:03 +0700

    update 06 - 11 - kt

--- src/app/main/landlord/index/index.component.ts
+++ src/app/main/landlord/index/index.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
+import { DataService } from 'src/app/core/services/data.service';
+import { NotificationService } from 'src/app/core/services/notification.service';
+import {NgbModal, ModalDismissReasons, NgbTooltipModule, NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @Component({
@@ -6,6 +11,69 @@ import { Component } from '@angular/core';
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
+  
+  constructor(
+    private _data : DataService,
+    private _notify : NotificationService,
+    private _modalService: NgbModal
+  ){} 
+
+
+  public chart: any;
+  public roomChart: any;
+
+  public statistic: any ={};
+
+  ngOnInit(): void {
+    this.loadData();
+    
+  }
+
+  loadDataToRoomChart(){
+    this.roomChart = new Chart("myPieChart", {
+      type: 'pie', //this denotes tha type of chart
+
+      data: {
+        labels: [
+          'Đang cho thuê',
+          'Trống',
+         
+        ],
+        datasets: [{
+          label: 'số phòng',
+          data: this.statistic.rentalRoom ,
+          backgroundColor: [
+            '#4e73df',
+            '#858796',
+          ],
+          hoverOffset: 4
+        }]
+      },
+      options: {
+        aspectRatio:2.5
+      }
+      
+    });
+
+  }
+
+
+  // load data to room info
+  public loadData(){
+    this._data.get('/api/appstatistic/general').subscribe(
+      {
+        next: res => {
+          console.log(res);
+          this.statistic = res;
+          this.loadDataToRoomChart();
+            
+        },
+        error: err => { this._data.handleError(err); console.log(err); },
+        complete: () => {  },
+      }
+    );
+  }
+
 
 }
